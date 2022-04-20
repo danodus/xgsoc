43b94aa0d3bcd1952aa2cfcf5dbf3b5754dbf6c5
Daniel Cliche <dancliche@gmail.com>
2022-04-20 10:25:00 -0400

    Add basic backspace

--- examples/basic/xio.c
+++ examples/basic/xio.c
@@ -33,27 +33,47 @@ void xscroll()
     g_cursor_y = 0;
 }
 
+void xprint_chr_xy(int x, int y, char c)
+{
+    xm_setw(WR_INCR, 1);    
+    xm_setw(WR_ADDR, y * NB_COLS + x);
+    xm_setw(DATA, 0x0F00 | c);
+}
+
 void xprint_chr(char c)
 {
-    if (c == '\n') {
-        g_cursor_x = 0;
-        g_cursor_y++;
-    }
+    if (c == '\b') {
+        // Backspace
+        
+        if (g_cursor_x == 0) {
+            if (g_cursor_y > 0) {
+                g_cursor_y--;
+                g_cursor_x = NB_COLS - 1;
+            }
+        } else {
+            g_cursor_x--;
+        }
 
-    if (g_cursor_y >= 29) {
-        xscroll();
-        g_cursor_x = 0;
-    }
-
-    if (c != '\r' && c != '\n') {
-        xm_setw(WR_INCR, 1);    
-        xm_setw(WR_ADDR, g_cursor_y * NB_COLS + g_cursor_x);
-        xm_setw(DATA, 0x0F00 | c);
-
-        g_cursor_x++;
-        if (g_cursor_x > 79) {
+        xprint_chr_xy(g_cursor_x, g_cursor_y, ' ');
+    } else {
+        if (c == '\n') {
             g_cursor_x = 0;
             g_cursor_y++;
+        }
+
+        if (g_cursor_y >= 29) {
+            xscroll();
+            g_cursor_x = 0;
+        }
+
+        if (c != '\r' && c != '\n') {
+            xprint_chr_xy(g_cursor_x, g_cursor_y, c);
+
+            g_cursor_x++;
+            if (g_cursor_x > NB_COLS - 1) {
+                g_cursor_x = 0;
+                g_cursor_y++;
+            }
         }
     }
 }
@@ -71,15 +91,23 @@ size_t xreadline(char *s, size_t buffer_len)
     size_t len = 0;
     while(len < buffer_len - 1) {
         char c = getch();
-        if (c == '\r') {
+        if (c == '\b') {
+            if (len > 0) {
+                s--;
+                *s = '\0';
+                len--;
+                xprint_chr(c);
+            }
+        } else if (c == '\r') {
             xprint_chr('\n');
             *s = '\0';
             return len;
+        } else {
+            xprint_chr(c);
+            *s = c;
+            s++;
+            len++;
         }
-        xprint_chr(c);
-        *s = c;
-        s++;
-        len++;
     }
 
     *s = '\0';
