b30c6a0ec786d50bd41c56dcee107b3492ec3ede
Daniel Cliche <dancliche@gmail.com>
2022-07-18 18:56:22 -0400

    Add fsdir and fsdelete calls to Lua

--- examples/lua/program.c
+++ examples/lua/program.c
@@ -1,4 +1,5 @@
 #include <io.h>
+#include <fs.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -13,7 +14,7 @@ int poke(lua_State *L) {
     unsigned int addr = luaL_checkinteger(L, 1);
     unsigned int value = luaL_checkinteger(L, 2);
     MEM_WRITE(addr, value);
-    return 1;
+    return 0;
 }
 
 int peek(lua_State *L) {
@@ -27,7 +28,7 @@ int call(lua_State *L) {
     unsigned int addr = luaL_checkinteger(L, 1);
     void (*fun_ptr)(void) = (void (*)(void))addr;
     (*fun_ptr)();
-    return 1;
+    return 0;
 }
 
 int callr(lua_State *L) {
@@ -43,7 +44,7 @@ int callp1(lua_State *L) {
     int p0 = luaL_checkinteger(L, 2);
     void (*fun_ptr)(int) = (void (*)(int))addr;
     (*fun_ptr)(p0);
-    return 1;
+    return 0;
 }
 
 
@@ -56,10 +57,64 @@ int callp1r(lua_State *L) {
     return 1;
 }
 
-
 int edit(lua_State *L) {
     const char *filename = luaL_checkstring(L, 1);
     start_editor(filename);
+    return 0;
+}
+
+int fsdir(lua_State *L) {
+    sd_context_t sd_ctx;
+    if (!sd_init(&sd_ctx)) {
+        lua_pushnil(L);
+        return 1;
+    }
+    fs_context_t fs_ctx;
+    if (!fs_init(&sd_ctx, &fs_ctx)) {
+        lua_pushnil(L);
+        return 1;
+    }
+    lua_newtable(L);
+    uint16_t nb_files = fs_get_nb_files(&fs_ctx);
+    for (uint16_t i = 0; i < nb_files; ++i) {
+        fs_file_info_t file_info;
+        if (fs_get_file_info(&fs_ctx, i, &file_info)) {
+            lua_pushstring(L, file_info.name);
+            lua_newtable(L);
+
+            lua_pushstring(L, "size");
+            lua_pushinteger(L, file_info.size);
+            lua_settable(L, -3);
+
+            lua_pushstring(L, "fblock");
+            lua_pushinteger(L, file_info.first_block_table_index);
+            lua_settable(L, -3);
+
+            lua_settable(L, -3);
+        }
+    }
+
+    return 1;
+}
+
+int fsdelete(lua_State *L) {
+    const char *filename = luaL_checkstring(L, 1);
+
+    sd_context_t sd_ctx;
+    if (!sd_init(&sd_ctx)) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    fs_context_t fs_ctx;
+    if (!fs_init(&sd_ctx, &fs_ctx)) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    if (!fs_delete(&fs_ctx, filename)) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    lua_pushboolean(L, 1);
     return 1;
 }
 
@@ -103,6 +158,12 @@ void main(void) {
 
     lua_pushcfunction(L, edit);
     lua_setglobal(L, "edit");
+
+    lua_pushcfunction(L, fsdir);
+    lua_setglobal(L, "fsdir");
+
+    lua_pushcfunction(L, fsdelete);
+    lua_setglobal(L, "fsdelete");  
 
     printf("Ready\n");
 
