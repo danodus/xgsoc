b550ca68769c3a9bc05846e79f0f7081c08f10bc
Daniel Cliche <dancliche@gmail.com>
2023-03-05 13:05:14 -0500

    Add support for ecall and ebreak

--- examples/common/sys.h
+++ examples/common/sys.h
@@ -6,6 +6,8 @@
 #define SYS_TTY_MODE_RAW    0x1
 
 extern void (*irq1_handler)();
+extern void (*ecall_handler)();
+extern void (*ebreak_handler)();
 
 void sys_set_tty_mode(unsigned int mode);
 unsigned int sys_get_tty_mode();
